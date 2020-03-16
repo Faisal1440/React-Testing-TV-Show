127ebfd1bdb4144067bbf7af6351645f95378c89
Faisal Aziz <faziz6006@gmail.com>
2020-03-15 19:04:10 -0500

    mpv pending

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
 import { formatSeasons } from "./utils/formatSeasons";
+import { fetchShow } from './api/fetchShow';
 
 import Episodes from "./components/Episodes";
 import "./styles.css";
+
+  // tests for  App.js nd Episodes.js components.
 
 export default function App() {
   const [show, setShow] = useState(null);
